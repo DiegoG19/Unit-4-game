d81a4751ee58ddd7bf5a1a6b5c701366499a523d
Diego G <gd369234@gmail.com>
2019-07-05 15:37:04 -0500

    Almost done only debugging some stuff

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,10 +4,10 @@ $(document).ready(function(){
 // Displays the random number in id randNum in the html
     $('#randNum').text(Random); 
 // selects the numbers for the four buttons being between(1-12)
-    var num1 = Math.floor(Math.random()*11+1)
-    var num2 = Math.floor(Math.random()*11+1)
-    var num3 = Math.floor(Math.random()*11+1)
-    var num4 = Math.floor(Math.random()*11+1)
+    var num1= Math.floor(Math.random()*11+1)
+    var num2= Math.floor(Math.random()*11+1)
+    var num3= Math.floor(Math.random()*11+1)
+    var num3= Math.floor(Math.random()*11+1)
 // set the variubles for the wins/loss/usertotal
 var userTotal= 0;
 var wins= 0;
@@ -19,11 +19,11 @@ $('#losses').text(losses);
 function reset() {
     Random = Math.floor(Math.random()*101+19)
     $('#randNum').text(Random);
-    num1 = Math.floor(Math.random()*11+1)
-    num2 = Math.floor(Math.random()*11+1)
-    num3 = Math.floor(Math.random()*11+1)
-    num4 = Math.floor(Math.random()*11+1)
-    userTotal = 0;
+    num1= Math.floor(Math.random()*11+1)
+    num2= Math.floor(Math.random()*11+1)
+    num3= Math.floor(Math.random()*11+1)
+    num3= Math.floor(Math.random()*11+1)
+    userTotal= 0;
     $('finaltotal').text(userTotal)
 }
 function win(){
@@ -77,6 +77,5 @@ $('#one').on ('click', function(){
              loss();
          }
         });
-        
 });
     
